777d2b9ad0388489d4c9a148b8dab9fe52fe6b00
Leonardo Souza <leogsouza@gmail.com>
2018-02-13 22:48:57 -0200

    App FirstLook: Add onclick method to button

--- FirstLook/app/src/main/java/br/com/leogsouza/firstlook/MainActivity.java
+++ FirstLook/app/src/main/java/br/com/leogsouza/firstlook/MainActivity.java
@@ -2,12 +2,28 @@ package br.com.leogsouza.firstlook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mButton;
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButton =  findViewById(R.id.mButton);
+        mTextView = findViewById(R.id.mTextView);
+
+        mButton.setText(R.string.button_name);
+    }
+
+    public void ShowMe(View view) {
+        mTextView.setVisibility(View.VISIBLE);
+        mTextView.setText(R.string.show_text);
     }
 }
