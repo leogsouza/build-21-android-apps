f6e497701a00ea369d7f372a614faa0a475bbfe8
Leonardo Souza <leogsouza@gmail.com>
2018-02-14 22:07:12 -0200

    App FirstLook: Show TextView with EditText content

--- FirstLook/app/src/main/java/br/com/leogsouza/firstlook/MainActivity.java
+++ FirstLook/app/src/main/java/br/com/leogsouza/firstlook/MainActivity.java
@@ -4,12 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button mButton;
     private TextView mTextView;
+    private EditText mEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +20,18 @@ public class MainActivity extends AppCompatActivity {
 
         mButton =  findViewById(R.id.mButton);
         mTextView = findViewById(R.id.mTextView);
+        mEditText = findViewById(R.id.mEditText);
 
         mButton.setText(R.string.button_name);
 
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                String enteredText;
+                enteredText = mEditText.getText().toString();
                 mTextView.setVisibility(View.VISIBLE);
-                mTextView.setText(R.string.show_text);
+                mTextView.setText(enteredText);
             }
         });
     }
