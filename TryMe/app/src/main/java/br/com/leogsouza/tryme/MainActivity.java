647a63688db67691ee00bd0d63d9a046af1df128
Leonardo Souza <leogsouza@gmail.com>
2018-02-25 12:58:55 -0300

    App TryMe: Add functionality to change Background Color

--- TryMe/app/src/main/java/br/com/leogsouza/tryme/MainActivity.java
+++ TryMe/app/src/main/java/br/com/leogsouza/tryme/MainActivity.java
@@ -1,13 +1,41 @@
 package br.com.leogsouza.tryme;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private View windowView;
+    private Button btnTryMe;
+    private int[] colors;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        colors = new int[]{Color.CYAN, Color.GREEN, Color.RED, Color.BLUE, Color.BLACK, Color.DKGRAY,
+                        Color.LTGRAY, Color.MAGENTA, Color.YELLOW};
+
+        windowView = findViewById(R.id.windowViewId);
+
+        windowView.setBackgroundColor(colors[2]);
+
+        btnTryMe = findViewById(R.id.btnTryMe);
+        btnTryMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int colorArrayLength = colors.length;
+                Random random = new Random();
+                int randomNum = random.nextInt(colorArrayLength);
+                windowView.setBackgroundColor(colors[randomNum]);
+            }
+        });
     }
 }
