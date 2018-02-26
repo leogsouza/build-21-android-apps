26dabe3d94520a9e8f60c16c701483f06f4ffa90
Leonardo Souza <leogsouza@gmail.com>
2018-02-25 23:07:36 -0300

    App SeekBar: Display Pain Level

--- SeekBar/app/src/main/java/br/com/leogsouza/seekbar/MainActivity.java
+++ SeekBar/app/src/main/java/br/com/leogsouza/seekbar/MainActivity.java
@@ -1,13 +1,41 @@
 package br.com.leogsouza.seekbar;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SeekBar seekBar;
+    private TextView resultTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        seekBar = findViewById(R.id.seekBar);
+        resultTextView = findViewById(R.id.resultId);
+        resultTextView.setText("Pain Level: " + seekBar.getProgress() + "/" + seekBar.getMax());
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                resultTextView.setTextColor(Color.DKGRAY);
+                resultTextView.setText("Pain Level: " + seekBar.getProgress() + "/" + seekBar.getMax());
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                if (seekBar.getProgress() >= 7) {
+                    resultTextView.setTextColor(Color.RED);
+                }
+            }
+        });
     }
 }
