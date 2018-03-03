10a4e6825887657ba76d91e00be32b207a060978
Leonardo Souza <leogsouza@gmail.com>
2018-03-03 10:19:02 -0300

    App ToggleButton: Hide and Seek Message

--- ToggleButton/app/src/main/java/br/com/leogsouza/togglebutton/MainActivity.java
+++ ToggleButton/app/src/main/java/br/com/leogsouza/togglebutton/MainActivity.java
@@ -2,12 +2,34 @@ package br.com.leogsouza.togglebutton;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ToggleButton toggleButton;
+    private TextView resultView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        resultView = findViewById(R.id.peekABooTextView);
+
+        toggleButton = findViewById(R.id.toggleButtonID);
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+
+                if(b) {
+                    resultView.setVisibility(View.VISIBLE);
+                } else {
+                    resultView.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
     }
 }
