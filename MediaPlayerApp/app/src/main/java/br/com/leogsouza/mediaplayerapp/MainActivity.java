ecde91d094f3e4a763078d32fc959ed16dc23123
Leonardo Souza <leogsouza@gmail.com>
2018-12-23 19:13:46 -0200

    App MediaPlayer - Playing a MP3 file

--- MediaPlayerApp/app/src/main/java/br/com/leogsouza/mediaplayerapp/MainActivity.java
+++ MediaPlayerApp/app/src/main/java/br/com/leogsouza/mediaplayerapp/MainActivity.java
@@ -1,13 +1,48 @@
 package br.com.leogsouza.mediaplayerapp;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+    private Button playButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.game_field);
+
+        playButton = findViewById(R.id.playButton);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mediaPlayer.isPlaying()) {
+                    pauseMusic();
+                } else {
+                    startMusic();
+                }
+            }
+        });
+    }
+
+    public void startMusic() {
+        if (mediaPlayer != null) {
+            mediaPlayer.start();
+            playButton.setText("Pause");
+        }
+    }
+
+    public void pauseMusic() {
+        if (mediaPlayer != null) {
+            mediaPlayer.pause();
+            playButton.setText("Play");
+        }
     }
 }
