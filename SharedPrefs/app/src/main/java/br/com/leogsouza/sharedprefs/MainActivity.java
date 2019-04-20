c06c24ede93f50d9fe87cd6175ea83bd56b3d0e4
Leonardo Souza <leogsouza@gmail.com>
2019-04-20 18:12:17 -0300

    App SharedPrefs - Finish the App

--- SharedPrefs/app/src/main/java/br/com/leogsouza/sharedprefs/MainActivity.java
+++ SharedPrefs/app/src/main/java/br/com/leogsouza/sharedprefs/MainActivity.java
@@ -1,13 +1,46 @@
 package br.com.leogsouza.sharedprefs;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button saveButton;
+    private TextView result;
+    private EditText enterMessage;
+    private SharedPreferences myPrefs;
+    private static final String PREFS_NAME = "myPrefsFile";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        enterMessage = findViewById(R.id.enterName);
+        result = findViewById(R.id.resultTextView);
+        saveButton = findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myPrefs = getSharedPreferences(PREFS_NAME, 0);
+                SharedPreferences.Editor editor = myPrefs.edit();
+
+                editor.putString("message", enterMessage.getText().toString());
+                editor.commit();
+            }
+        });
+
+        // Get data bacl
+        SharedPreferences prefs = getSharedPreferences(PREFS_NAME, 0);
+
+        if (prefs.contains("message")) {
+            String message = prefs.getString("message", "not found");
+            result.setText(String.format("Message: %s", message));
+        }
     }
 }
