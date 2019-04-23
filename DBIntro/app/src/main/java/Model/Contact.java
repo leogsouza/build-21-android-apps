347428de76455915fd21e27d37cccbf68ba8ce88
Leonardo Souza <leogsouza@gmail.com>
2019-04-23 20:51:06 -0300

    App Contact Manager - Create Model and DatabaseHandler

---
+++ DBIntro/app/src/main/java/Model/Contact.java
@@ -0,0 +1,15 @@
+package Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Contact {
+
+    private int id;
+    private String name;
+    private String phoneNumber;
+}
