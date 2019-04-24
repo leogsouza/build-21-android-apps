2abebd565d64029e9bc4b9df51a45c7afbcf0093
Leonardo Souza <leogsouza@gmail.com>
2019-04-23 21:17:23 -0300

    App Contact Manager - AddContact and GetContact methods

--- DBIntro/app/src/main/java/Data/DatabaseHandler.java
+++ DBIntro/app/src/main/java/Data/DatabaseHandler.java
@@ -1,7 +1,10 @@
 package Data;
 
+import Model.Contact;
 import Utils.Util;
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.annotation.Nullable;
@@ -14,7 +17,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     // Create tables
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        String CREATE_CONTACT_TABLE = "CREATE_TABLE " + Util.TABLE_NAME + "("
+        String CREATE_CONTACT_TABLE = "CREATE TABLE " + Util.TABLE_NAME + "("
                 + Util.KEY_ID + " INTEGER PRIMARY KEY, "
                 + Util.KEY_NAME + " TEXT, " + Util.KEY_PHONE_NUMBER +" TEXT)";
 
@@ -28,6 +31,33 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
         // Create table aguain
         onCreate(sqLiteDatabase);
+    }
 
+    public void addContact(Contact contact) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues value =  new ContentValues();
+        value.put(Util.KEY_NAME, contact.getName());
+        value.put(Util.KEY_PHONE_NUMBER, contact.getPhoneNumber());
+
+        // Insert to row
+        db.insert(Util.TABLE_NAME, null, value);
+        db.close();
+    }
+
+    // Get a contact
+    public Contact getContact(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String[] columns = {Util.KEY_ID, Util.KEY_NAME, Util.KEY_PHONE_NUMBER};
+        Cursor cursor = db.query(Util.TABLE_NAME, columns, Util.KEY_ID + "=?",
+                new String[]{String.valueOf(id)}, null, null, null, null);
+
+        if (cursor != null)
+            cursor.moveToFirst();
+
+        Contact contact = new Contact(Integer.parseInt(cursor.getString(0)),
+                cursor.getString(1), cursor.getString(2));
+
+        return contact;
     }
 }
