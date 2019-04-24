5ccb7e63ce47e314683f0322decfeb8d3936c66d
Leonardo Souza <leogsouza@gmail.com>
2019-04-23 21:00:03 -0300

    App Contact Manager - Create contact table

--- DBIntro/app/src/main/java/Data/DatabaseHandler.java
+++ DBIntro/app/src/main/java/Data/DatabaseHandler.java
@@ -1,22 +1,33 @@
 package Data;
 
+import Utils.Util;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.annotation.Nullable;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
-    public DatabaseHandler(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public DatabaseHandler(Context context ) {
+        super(context, Util.DATABASE_NAME, null, Util.DATABASE_VERSION);
     }
 
+    // Create tables
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String CREATE_CONTACT_TABLE = "CREATE_TABLE " + Util.TABLE_NAME + "("
+                + Util.KEY_ID + " INTEGER PRIMARY KEY, "
+                + Util.KEY_NAME + " TEXT, " + Util.KEY_PHONE_NUMBER +" TEXT)";
+
+        sqLiteDatabase.execSQL(CREATE_CONTACT_TABLE);
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + Util.TABLE_NAME);
+
+        // Create table aguain
+        onCreate(sqLiteDatabase);
 
     }
 }
